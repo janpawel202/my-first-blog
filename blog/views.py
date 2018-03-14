a9d3269a675732f3d5ec972608852014ffa9c7b2
Darek G <dariusz.grupa@gmail.com>
2018-03-14 20:31:43 +0100

    Lista postów z bazy danych

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
